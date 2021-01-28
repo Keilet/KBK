66419782d9fd170d51e243cd29917570cae0f7f6
Andrews <rakdav@inbox.ru>
2021-01-28 11:06:41 +0300

    escho raz proverim

--- app/src/main/java/com/example/kbk/EnterteActivity.kt
+++ app/src/main/java/com/example/kbk/EnterteActivity.kt
@@ -2,14 +2,22 @@ package com.example.kbk
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.textfield.TextInputEditText
 
 
 class EnterteActivity : AppCompatActivity(){
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.enterte)
+
+        val inlogin: TextInputEditText = findViewById(R.id.login)
+        val inpass: TextInputEditText = findViewById(R.id.pass)
 
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
@@ -18,6 +26,8 @@ class EnterteActivity : AppCompatActivity(){
         toolbar.setNavigationOnClickListener {
             onBackPressed() // возврат на предыдущий activity
         }
+
+
     }
 
 }

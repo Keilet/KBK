632178dca16311747813102a57b7aaf8e51992dc
Dani M <johndoe@example.com>
2021-04-08 17:31:20 +0200

    dima

--- app/src/main/java/com/example/kbk/activities/EnterteActivity.kt
+++ app/src/main/java/com/example/kbk/activities/EnterteActivity.kt
@@ -46,14 +46,15 @@ class EnterteActivity : AppCompatActivity(), View.OnClickListener {
         val login: String = inlogin.text.toString().trim()
         val pass: String = inpass.text.toString().trim()
 
-        val retrofit=Retrofit.Builder()
+/*        val retrofit=Retrofit.Builder()
                 .baseUrl(ServiceBuilder.URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-        val service:Api = retrofit.create(Api::class.java)
+        val service:Api = retrofit.create(Api::class.java)*/
 
-        val call: Call<LoginResponse> = service.userlogin(login, pass)
 
+        //val call: Call<LoginResponse> = RetrofitClient.service.userlogin(login, pass)
+        val call: Call<LoginResponse> = service.fetchContents()
 
 
 
@@ -62,15 +63,16 @@ class EnterteActivity : AppCompatActivity(), View.OnClickListener {
                 Log.e(TAG, "Failed to fetch photos", t)
                //Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
             }
-            override fun onResponse(call: Call<LoginResponse>?, response: Response<LoginResponse>) {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                Log.e(TAG, "Response received:${response.body()}")
                 //progressDialog.dismiss()
-                if (!response.body().error()) {
-                    finish()
-                    SharedPrefManager.getInstance(applicationContext).userLogin(response.body().getUser())
-                    startActivity(Intent(applicationContext, HomeActivity::class.java))
-                } else {
-                    Toast.makeText(applicationContext, "Invalid email or password", Toast.LENGTH_LONG).show()
-                }
+//                if (!response.body().error()) {
+//                    finish()
+//                    SharedPrefManager.getInstance(applicationContext).userLogin(response.body().getUser())
+//                    startActivity(Intent(applicationContext, HomeActivity::class.java))
+//                } else {
+//                    Toast.makeText(applicationContext, "Invalid email or password", Toast.LENGTH_LONG).show()
+//                }
             }
 
 
